98ab7bfd16966be1233096d0cad32c3233febe70
anugrahp13 <anugrahprastyo13@gmail.com>
2025-03-22 19:32:13 +0700

    feat[index.jsx]: create view dashboard

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,24 +2,31 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 
 //import store
-import { useStore } from '../stores/user';
+import { useStore } from "../stores/user";
 
 //import view login
 import Login from "../views/auth/login.jsx";
 
-export default function AppRoutes() {
+//import view dashboard
+import Dashboard from "../views/dashboard/index.jsx";
 
+export default function AppRoutes() {
   //destruct state "token" from store
   const { token } = useStore();
 
   return (
     <Routes>
-
       {/* route "/" */}
-      <Route path="/" element={
-        token ? <Navigate to="/dashboard" replace /> : <Login />
-      } />
+      <Route
+        path="/"
+        element={token ? <Navigate to="/dashboard" replace /> : <Login />}
+      />
 
+      {/* route "/dashboard" */}
+      <Route
+        path="/dashboard"
+        element={token ? <Dashboard /> : <Navigate to="/" replace />}
+      />
     </Routes>
   );
 }
