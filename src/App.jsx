ad85cf1cf3517c3dfd323e9c8ce8b767f2101e37
anugrahp13 <anugrahprastyo13@gmail.com>
2025-03-22 19:19:07 +0700

    refactor[App.jsx]: showing theme in project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
+// Import React and useEffect
+import React, { useEffect } from 'react';
+
 //import Toaster
-import { Toaster } from "react-hot-toast";
+import { Toaster } from 'react-hot-toast';
+
+// Import theme store
+import { useStore } from './stores/theme';
 
 function App() {
+
+  // destructure state "theme" from useStore
+  const { theme } = useStore();
+
+  // set document theme
+  useEffect(() => {
+    document.documentElement.setAttribute('data-bs-theme', theme);
+  }, [theme]);
+
   return (
     <>
       <Toaster />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
