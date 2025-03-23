79bff01b0e8866242bbda37e39548905303fa087
anugrahp13 <anugrahprastyo13@gmail.com>
2025-03-23 20:16:48 +0700

    refactor[index.jsx]: create route customers

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,7 +2,7 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 
 //import store
-import { useStore } from '../stores/user';
+import { useStore } from "../stores/user";
 
 //import view login
 import Login from "../views/auth/login.jsx";
@@ -16,33 +16,44 @@ import CategoriesIndex from "../views/categories/index.jsx";
 //import view products
 import ProductsIndex from "../views/products/index.jsx";
 
-export default function AppRoutes() {
+//import view customers
+import CustomersIndex from "../views/customers/index.jsx";
 
+export default function AppRoutes() {
   //destruct state "token" from store
   const { token } = useStore();
 
   return (
     <Routes>
-
       {/* route "/" */}
-      <Route path="/" element={
-        token ? <Navigate to="/dashboard" replace /> : <Login />
-      } />
+      <Route
+        path="/"
+        element={token ? <Navigate to="/dashboard" replace /> : <Login />}
+      />
 
       {/* route "/dashboard" */}
-      <Route path="/dashboard" element={
-        token ? <Dashboard /> : <Navigate to="/" replace />
-      } />
+      <Route
+        path="/dashboard"
+        element={token ? <Dashboard /> : <Navigate to="/" replace />}
+      />
 
       {/* route "/categories" */}
-      <Route path="/categories" element={
-        token ? <CategoriesIndex /> : <Navigate to="/" replace />
-      } />
+      <Route
+        path="/categories"
+        element={token ? <CategoriesIndex /> : <Navigate to="/" replace />}
+      />
 
       {/* route "/products" */}
-      <Route path="/products" element={
-        token ? <ProductsIndex /> : <Navigate to="/" replace />
-      } />
+      <Route
+        path="/products"
+        element={token ? <ProductsIndex /> : <Navigate to="/" replace />}
+      />
+
+      {/* route "/customers" */}
+      <Route
+        path="/customers"
+        element={token ? <CustomersIndex /> : <Navigate to="/" replace />}
+      />
     </Routes>
   );
 }
