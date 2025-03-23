981e76398fa9a57f776c47d2af6abea613d8d276
anugrahp13 <anugrahprastyo13@gmail.com>
2025-03-24 02:24:14 +0700

    refactor[index.jsx]: create route sales

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,7 +2,7 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 
 //import store
-import { useStore } from "../stores/user";
+import { useStore } from '../stores/user';
 
 //import view login
 import Login from "../views/auth/login.jsx";
@@ -25,53 +25,64 @@ import UsersIndex from "../views/users/index.jsx";
 //import view transactions
 import TransactionsIndex from "../views/transactions/index.jsx";
 
+//import view print
+import Print from "../views/transactions/print/print.jsx";
+
+//import view sales index
+import SalesIndex from "../views/sales/index.jsx";
+
 export default function AppRoutes() {
+
   //destruct state "token" from store
   const { token } = useStore();
 
   return (
     <Routes>
+
       {/* route "/" */}
-      <Route
-        path="/"
-        element={token ? <Navigate to="/dashboard" replace /> : <Login />}
-      />
+      <Route path="/" element={
+        token ? <Navigate to="/dashboard" replace /> : <Login />
+      } />
 
       {/* route "/dashboard" */}
-      <Route
-        path="/dashboard"
-        element={token ? <Dashboard /> : <Navigate to="/" replace />}
-      />
+      <Route path="/dashboard" element={
+        token ? <Dashboard /> : <Navigate to="/" replace />
+      } />
 
       {/* route "/categories" */}
-      <Route
-        path="/categories"
-        element={token ? <CategoriesIndex /> : <Navigate to="/" replace />}
-      />
+      <Route path="/categories" element={
+        token ? <CategoriesIndex /> : <Navigate to="/" replace />
+      } />
 
       {/* route "/products" */}
-      <Route
-        path="/products"
-        element={token ? <ProductsIndex /> : <Navigate to="/" replace />}
-      />
+      <Route path="/products" element={
+        token ? <ProductsIndex /> : <Navigate to="/" replace />
+      } />
 
       {/* route "/customers" */}
-      <Route
-        path="/customers"
-        element={token ? <CustomersIndex /> : <Navigate to="/" replace />}
-      />
+      <Route path="/customers" element={
+        token ? <CustomersIndex /> : <Navigate to="/" replace />
+      } />
 
       {/* route "/users" */}
-      <Route
-        path="/users"
-        element={token ? <UsersIndex /> : <Navigate to="/" replace />}
-      />
+      <Route path="/users" element={
+        token ? <UsersIndex /> : <Navigate to="/" replace />
+      } />
 
       {/* route "/transactions" */}
-      <Route
-        path="/transactions"
-        element={token ? <TransactionsIndex /> : <Navigate to="/" replace />}
-      />
+      <Route path="/transactions" element={
+        token ? <TransactionsIndex /> : <Navigate to="/" replace />
+      } />
+
+      {/* route "/transactions/print" */}
+      <Route path="/transactions/print" element={
+        token ? <Print /> : <Navigate to="/" replace />
+      } />
+
+      {/* route "/sales" */}
+      <Route path="/sales" element={
+        token ? <SalesIndex /> : <Navigate to="/" replace />
+      } />
     </Routes>
   );
 }
